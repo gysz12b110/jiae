5735cb8e9dfb20209bfbd9cd06aa2008a1915f4c
jingjing <2298044627@qq.com>
2017-05-18 21:17:32 +0800

    delete

---
+++ JIAE/app/public/assets/js/util.js
@@ -0,0 +1,82 @@
+//alert
+var $msgmodal = $('#jiae-alert');
+var $msg2modal = $("#jiae-confirm");
+var $zeModal = $('#ze-modal');
+
+function openAlert(msg){
+	$("#alertMsg").html(msg);
+	$msgmodal.modal('open');
+}
+
+function openConfirm(title, msg, surecallback, cancelCallback){
+	$("#confirmTitle").html(title);
+	$("#confirmMsg").html(msg);
+	$msg2modal.modal({
+        relatedTarget: this,
+        onConfirm: function(options) {
+        	//surecallback();
+        },
+        // closeOnConfirm: false,
+        onCancel: function() {
+        	//cancelCallback();
+        }
+     });
+	  var $confirmBtn = $msg2modal.find('[data-am-modal-confirm]');
+	  var $cancelBtn = $msg2modal.find('[data-am-modal-cancel]');
+	  $confirmBtn.off('click.confirm.modal.amui').on('click', function() {
+		  surecallback();
+	  });
+
+	  $cancelBtn.off('click.cancel.modal.amui').on('click', function() {
+		  cancelCallback();
+	  });
+}
+
+
+Array.prototype.remove=function(dx) 
+{
+    if(isNaN(dx)||dx>this.length){return false;} 
+    for(var i=0,n=0;i<this.length;i++) 
+    { 
+        if(this[i]!=this[dx]) 
+        { 
+            this[n++]=this[i] 
+        } 
+    } 
+    this.length-=1 
+}; 
+
+function showWarning(id,msg){
+	$(id).popover({
+	    content: msg
+	  });
+}
+$(function(){
+	$("img.lazy").lazyload({
+		effect : 'fadeIn'
+	});
+});
+
+
+function addCart() {
+	var btn = $(".j-btn-catalog");
+	btn.each(function(){
+		$(this).on("click", function(){addSaleCart($(this));});
+	});
+	
+	function addSaleCart(clickPid) {
+		$.ajax({
+            url: "knockcart",
+            type: "post",
+            data: {
+            	productId: clickPid.attr("data-id")
+            }
+        }).done(function(e) {
+        	$("#J-cart-num").text(e.quantity),
+        	e.quantity == 0?$("#J-cart-num").css("display", "none"): $("#J-cart-num").css("display", "block"),
+        	openAlert("鍔犲叆璐墿杞︽垚鍔燂紒");
+        }).fail(function(e) {
+        	openAlert("鍔犲叆璐墿杞﹀け璐ワ紒");
+        });
+	}
+}
