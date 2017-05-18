fc4cd551a7330ee45a3d8214aa8ab31b54d419ca
sun_sy <253566958@qq.com>
2017-05-18 21:20:31 +0800

    sy

--- JIAE/manage/admin/js/goodstype.js
+++ JIAE/manage/admin/js/goodstype.js
@@ -30,39 +30,9 @@ var app=angular.module("myapp",[]);
 			//发起ajax请求,从回调函数的参数中拿到结果,这个结果是个数组
 			$scope.httpGet("list",{})
 			
-			//删除商品
-			$scope.delgoods=function(id){
-				alert(123);
-				$scope.httpPost("delgoods",{id})
-			}
-			
-			//保存商品(更新、添加)
-			$scope.savegoods=function(goods){
-				console.log(goods);
-				if(goods.id && goods.id!=""){
-					$scope.httpPost("updategoods",goods);
-				}else{
-					$scope.httpPost("addgoods",goods);
-				}
-			}
-			
-			//搜索
-			$scope.search=function(){
-				console.warn($scope.goodsname+"/"+$scope.sex);
-				$scope.httpGet("list",{name:$scope.goodsname,sex:$scope.sex});
-			}
-			
+		
 			
 		}])
 		
 		
-		//添加一行
-		$("#addBtn").click(function() {
-			var td0 = $("<td><input type='checkbox'></td>");
-			var td1 = $("<td>{{goods.id}}</td>");
-			var td2 = $("<td><input ng-model='goods.name'  class='Iput'></td>");
-			var td3 = $("<td> <span class = 'fa fa-file  savegoods'></span><span class = 'fa fa-pencil-square-o  updategoods'></span> <span class = 'fa fa-trash  delgoods'></span> </td>");
-			var tr = $("<tr></tr>"); 
-			tr.append(td0).append(td1).append(td2).append(td3).appendTo("tbody");
-		})
-		
+	
--- JIAE/manage/app.js
+++ JIAE/manage/app.js
@@ -5,6 +5,7 @@ var bodyParser = require("body-parser"); //URL解析库
 var pools = require("./server/utils/pool"); //工具类
 var tools = require("./server/utils/tools");
 var userRoutes = require("./server/controller/userController");
+var goodsRoutes = require("./server/controller/goodsController");
 var MessageXSend = require('./server/sms/messageXSend.js');
 
 var urlParser = bodyParser.urlencoded({ extended: false });
@@ -16,6 +17,8 @@ app.get("/", function(req, res) {
 
 //用户路由
 userRoutes(app, urlParser, tools,pools);
+goodsRoutes(app, urlParser, tools,pools);
+
 app.listen(9000);
 console.log("server start in 9000");
 
@@ -35,63 +38,3 @@ app.post("/sms",urlParser,function(req,res){
 });
 
 
-/**
- * 查询所有商品
- */
-app.get("/list",function(req,res){
-    //GET请求：获取参数使用query
-    var name=req.query.name;
-    var sex=req.query.sex;
-    var sql="select id,name from goodstype where 1=1 and parent_id=0 ";
-    if(name &&　name!="")  sql+=" and name like '%"+name+"%' ";
-    console.log(sql);
-    pools(sql,[],function(result){
-        res.send(result);
-    })
-})
-
-/**
- * 添加新商品
- */
-app.post("/addgoods",urlParser,function(req,res){
-    var sql="insert into goodstype(name,age,sex)values(?)";
-    var param=[req.body.name,];
-    pools(sql,param,function(result){
-        if(result.affectedRows>0){
-            res.send("success");
-        }else{
-            res.send("failure");
-        }
-    })
-})
-
-
-/**
- * 删除商品
- */
-app.post("/delgoods",urlParser,function(req,res){
-    var sql="update goodstype set state=0 where id=?";
-    var param=[req.body.no];
-   pools(sql,param,function(result){
-        if(result.affectedRows>0){
-            res.send("success");
-        }else{
-            res.send("failure");
-        }
-    })
-})
-
-/**
- * 更新商品信息
- */
-app.post("/updateuser",urlParser,function(req,res){
-    var sql="update goodstype set name=? where id=?";
-    var param=[req.body.name,req.body.no];
-    pools(sql,param,function(result){
-        if(result.affectedRows>0){
-            res.send("success");
-        }else{
-            res.send("failure");
-        }
-    })
-})
