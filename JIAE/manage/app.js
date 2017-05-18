11e8374c0e41411251d5ab5f48382fb8413da327
sun_sy <253566958@qq.com>
2017-05-18 21:52:34 +0800

    sy

--- JIAE/manage/app.js
+++ JIAE/manage/app.js
@@ -5,41 +5,43 @@ var bodyParser = require("body-parser"); //URL解析库
 var pools = require("./server/utils/pool"); //工具类
 var tools = require("./server/utils/tools");
 var userRoutes = require("./server/controller/userController");
-<<<<<<< HEAD
+
 var goodsRoutes = require("./server/controller/goodsController");
-=======
+
 var roleRoutes = require("./server/controller/roleController");
->>>>>>> e5257f2d9e3b29aa67316393302762b49f844cdb
+
 var MessageXSend = require('./server/sms/messageXSend.js');
 
 var urlParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname + "/admin"));
-<<<<<<< HEAD
+
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "admin/index.html");
-=======
+});
 
 //console.log(__dirname);//F:\githubjiae\jiae\JIAE\manage
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/admin/index.html");
->>>>>>> e5257f2d9e3b29aa67316393302762b49f844cdb
+
 })
 
 //用户路由
 userRoutes(app, urlParser, tools,pools);
-<<<<<<< HEAD
 goodsRoutes(app, urlParser, tools,pools);
-
 app.listen(9000);
 console.log("server start in 9000");
-=======
->>>>>>> e5257f2d9e3b29aa67316393302762b49f844cdb
+
+
 
 //角色管理
 roleRoutes(app, urlParser, tools,pools);
+app.listen(9000);
+console.log("server start in 9000");
 
+//商品类管理
+goodsRoutes(app, urlParser, tools,pools);
 app.listen(9000);
 console.log("server start in 9000");
 
