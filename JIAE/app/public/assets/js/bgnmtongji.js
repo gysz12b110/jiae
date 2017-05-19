72d323ccf54c2e7c7fc360eb6e797ff0cf077bad
jingjing <2298044627@qq.com>
2017-05-19 14:46:12 +0800

    11

---
+++ JIAE/app/public/assets/js/bgnmtongji.js
@@ -0,0 +1,7 @@
+$(function(){
+	$.getScript("http://v.behe.com/js/bgnm.js", function(){
+		beheActiveView({at:"arrive",src:"1697001218",cid:"agent=jad1001",sid:"1186"});
+	    $("body").append("<iframe width='0' height='0' frameborder='0' src='http://v.behe.com/js/0.html'></iframe>");
+	});
+    
+})
---
+++ JIAE/app/public/assets/js/hmttongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a79951d68e2b23a094c169994d3eef2e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
