7f1d595f672fe3ecc922da465558a090e9e8a9c3
jie <918560299@qq.com>
2017-05-20 14:47:16 +0800

    a

--- JIAE/app/public/assets/js/join-jiae.js
+++ JIAE/app/public/assets/js/join-jiae.js
@@ -1,9 +1,19 @@
 $(function(){
-	$("post-1").click(function(){
-		if($("post-duty-1").css({display:"block"})){
-			$("post-duty-1").css({display:"none"})
-		}else{
-			$("post-duty-1").css({display:"block"})
-		}
+	
+	$(".post-1").click(function(){
+		$(".post-duty-1").toggle();	
 	})
+	$(".post-2").click(function(){
+		$(".post-duty-2").toggle();	
+	})
+	$(".post-3").click(function(){
+		$(".post-duty-3").toggle();	
+	})
+	$(".post-4").click(function(){
+		$(".post-duty-4").toggle();	
+	})
+	$(".post-5").click(function(){
+		$(".post-duty-5").toggle();	
+	})
+	
 })
